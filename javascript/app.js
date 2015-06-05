c22c66f1344dc6c56a1aca445c7a78469e2151ab
James Mack <github@wulph.com>
2015-06-05 02:25:52 -0700

    Most stuff works

--- javascript/app.js
+++ javascript/app.js
@@ -1,20 +1,18 @@
 
 /*
 TODO
-- recordButton toggle text and disable
-- playbackButton toggle text and disable
-- display geoData in table
 - write geoData to file
 - playback data from file
-
-
-playback: 
-have each set of gps data in array, iterate through the array, get time till since last action and timeout for that length
 */
 
 var toggleRecord = false;
 var togglePlayback = false;
+var readWriteFile = false;
+var debugOutput = false;
 
+var counter = 0;
+var geoDataLog = "";
+var geoDataArray = new Array();
 
 
 function toggleRecordClick()
@@ -24,29 +22,154 @@ function toggleRecordClick()
         toggleRecord = false;
         $("#button-toggle-record").text("Start Recording");
         $("#button-toggle-playback").prop("disabled", false);
+        $("#display-status").html("");
     }
     else {
         toggleRecord = true;
         $("#button-toggle-record").text("Stop Recording");
         $("#button-toggle-playback").prop("disabled", true);
+        $("#display-status").html("Recording");
+        updateGeoData();
     }
 
 }
 
 function togglePlaybackClick()
 {
-    //getGeoData();
     if(togglePlayback) {
         togglePlayback = false;
         $("#button-toggle-playback").text("Start Playback");
         $("#button-toggle-record").prop("disabled", false);
+        $("#display-status").html("");
     }
     else {
         togglePlayback = true;
         $("#button-toggle-playback").text("Stop Playback");
         $("#button-toggle-record").prop("disabled", true);
+        $("#display-status").html("Playback");
+        dataRead = false;
+        readGeoData();
+        //if(debugOutput) $("#message").html("");
     }
 
+}
+
+function displayGeoData(geoData)
+{
+    $("#display-longitude").html(geoData.longitude);
+    $("#display-latitude").html(geoData.latitude);
+    $("#display-heading").html(geoData.heading + " degrees");
+    $("#display-speed").html(geoData.speed + " m/s");
+
+    var entry = geoData.timestamp + " - Longitude: " + geoData.longitude + ", Latitude: " + geoData.latitude + ", Heading: " + geoData.heading + ", Speed: " + geoData.speed;
+    if(debugOutput) $("#message").append("<p>" + entry + "</p>");
+}
+
+function recordGeoData(geoData)
+{
+    var entry = geoData.timestamp + " - Longitude: " + geoData.longitude + ", Latitude: " + geoData.latitude + ", Heading: " + geoData.heading + ", Speed: " + geoData.speed + "\n";
+    geoDataLog += entry;
+}
+
+function writeGeoDataLog()
+{
+    // Create blog with the log data
+    var dataBlob = blackberry.utils.stringToBlob(geoDataLog, "UTF-8");
+
+    // Create directory for the log file
+    var fileDir = blackberry.io.dir.appDirs.app.storage.path + "/gps-data";
+    blackberry.io.dir.createNewDir(fileDir);
+
+    // Save log file
+    blackberry.io.file.saveFile(fileDir + "/GPS_data.log", dataBlob);
+}
+
+function readGeoData()
+{
+    if(readWriteFile) {
+        readGeoDataLog();
+    }
+    else {
+        parseGeoData(geoDataLog); 
+    }
+}
+
+function readGeoDataLog()
+{
+    // Get log file path and create filename
+    var fileDir = blackberry.io.dir.appDirs.app.storage.path + "/gps-data";
+    var filename = fileDir + "/GPS_data.log";
+
+    // If the log file exists, open the log file and specify callback to load the data
+    if(blackberry.io.file.exists(filename)) {
+        blackberry.io.file.readFile(filename, loadGeoDataLog);
+    }
+}
+
+function loadGeoDataLog(fullPath, dataBlob)
+{
+    // Populate geoDataLog with the string data from blob
+    geoDataLog = blackberry.utils.blobToString(dataBlob, "UTF-8");
+    parseGeoData(geoDataLog);
+}
+
+function parseGeoData(geoDataString)
+{
+    if(debugOutput) $("#message").html("");
+    // Split the string up around "\n"
+    var entries = geoDataString.split("\n");
+    if(debugOutput) $("#message").append("<p>Total Entries: " + entries.length + "</p>");
+
+    var regex = /(\d+) - Longitude: (-?\d+\.\d+), Latitude: (-?\d+\.\d+), Heading: (\d+|null), Speed: (\d+|null)/;
+
+    geoDataArray = new Array();
+
+    for(var i = 0; i < entries.length; ++i) {
+        if(entries[i].length == 0) continue;
+        var match = regex.exec(entries[i]);
+
+        // NOTE: match[0] is the whole string
+        var geoData = {
+            timestamp: match[1],
+            longitude: match[2],
+            latitude: match[3],
+            heading: match[4],
+            speed: match[5]
+        };
+        
+        geoDataArray.push(geoData);
+    }
+    playbackGeoData();
+}
+
+function playbackGeoData()
+{
+    if(togglePlayback) {
+        if(geoDataArray.length > 0) {
+            var geoData = geoDataArray[0];
+            geoDataArray.shift();
+            displayGeoData(geoData);
+            setTimeout(playbackGeoData, 1000);
+        }
+        else {
+            togglePlaybackClick();
+        }
+    }
+}
+
+
+function clearGeoDataLog()
+{
+    geoDataLog = "";
+}
+
+function updateGeoData()
+{
+    if(toggleRecord) {
+         
+        getGeoData();
+        setTimeout(updateGeoData, 1000);
+    }
 }
 
 function getGeoData()
@@ -58,26 +181,35 @@ function getGeoData()
       navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, options);
    }
    else {
-      alert("HTML5 geolocation is not supported.");
+      alert("No GPS data is available. Please check your phone.");
    }
 }
 
 function geolocationSuccess(position)
 {
-   //Extract information about the users current position:
-   var time = position.timestamp;
-   var coordinates = position.coords;
+    //Extract information about the users current position:
+    var timestamp = position.timestamp;
+    var coordinates = position.coords;
+    
+    // Retrieve geographic information about the GPS fix:
+    var latitude = coordinates.latitude;
+    var longitude = coordinates.longitude;
+    //var altitude = coordinates.altitude;
+    //var accuracy = coordinates.accuracy;
+    //var altitudeAccuracy = coordinates.altitudeAccuracy;
+    var heading = coordinates.heading;
+    var speed = coordinates.speed;
 
-   //Retrieve geographic information about the GPS fix:
-   var lat = coordinates.latitude;
-   var lon = coordinates.longitude;
-   var alt = coordinates.altitude;
-   var acc = coordinates.accuracy;
-   var altAcc = coordinates.altitudeAccuracy;
-   var head = coordinates.heading;
-   var speed = coordinates.speed;
-
-   alert("You are located at " + lat + ", " + lon);
+    var geoData = {
+        timestamp: timestamp,
+        latitude: latitude,
+        longitude: longitude,
+        heading: heading,
+        speed: speed
+    };
+    
+    displayGeoData(geoData);
+    recordGeoData(geoData);
 }
 
 function geolocationError(error)
